55649a747f58ad4b67e8a5b6d6d65a6be83a9bdc
RicardoBarrote <ricardoambscfc@gmail.com>
2025-06-17 19:21:39 -0300

    Iniciando o projeto

---
+++ src/test/java/com/tech/smartstock/SmartstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.smartstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
